2c9b3b7b550407b6135c5b5de76117cefb28e6fb
Truong Nam <nam.tv@vlu.edu.vn>
2025-08-29 11:09:13 +0700

    fix

--- pm2.client.config.js
+++ pm2.client.config.js
@@ -1,3 +1,14 @@
+/* eslint-disable @typescript-eslint/no-require-imports */
+const path = require("path");
+const dotenv = require("dotenv");
+
+const loadEnv = () => {
+  const envPath = path.resolve(__dirname, `.env`);
+  const result = dotenv.config({ path: envPath });
+  console.log("envPath", envPath);
+  return result.parsed || {};
+};
+
 module.exports = {
   apps: [
     {
@@ -6,7 +17,7 @@ module.exports = {
       script: ".next/standalone/server.js",
       exec_mode: "cluster",
       instances: 1,
-      env_file: "/home/test/repos/web-test/.env",
+      env: loadEnv(),
     },
   ],
 };
